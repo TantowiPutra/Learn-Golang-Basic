ebfc9ab0b5e7016a2bf7da29110565e8c6c3c55e
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-03-09 23:25:28 +0700

    GO LEARN: Added Menu Option

---
+++ main/boolean.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printBoolean() {
+	true_var  := true
+	false_var := false
+
+	fmt.Println(true_var)
+	fmt.Println(false_var)
+}
--- main/main.go
+++ main/main.go
@@ -1,7 +1,54 @@
 package main
 
+import (
+	"fmt"
+)
+
+type MenuItem struct {
+	idx  int    `int`
+	menu string `string`
+}
+
 // Basic Output to Console & Code Compilation using "go build" command
 func main() {
-	printHelloWorld()
-	printBasicNumbers()
+	var option int
+
+	menuList := []MenuItem{
+		{1, "Hello, World!"},
+		{2, "Basic Numbers"},
+		{3, "Boolean"},
+		{4, "Exit"},
+	}
+
+	for true {
+		printMenu(&menuList)
+
+		fmt.Printf("Select Option: ")
+		fmt.Scan(&option)
+
+		switch option {
+		case 1:
+			printHelloWorld()
+			fmt.Println()
+		case 2:
+			printBasicNumbers()
+			fmt.Println()
+		case 3:
+			printBoolean()
+			fmt.Println()
+		}
+
+		if option == 4 {
+			break
+		}
+	}
+}
+
+func printMenu(menuList *[]MenuItem) {
+	fmt.Println("Welcome Abord! Please Choose a Menu To Proceed: ")
+	fmt.Println("================================================================")
+
+	for _, item := range *menuList {
+		fmt.Printf("%d. %s\n", item.idx, item.menu)
+	}
 }
